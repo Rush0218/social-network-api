35e6bd99fda6b0b2f537c014447e44f682c1ad93
solera2 <rushtre1@gmail.com>
2022-02-14 00:20:26 -0500

    created models and utils dir for date formatting

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, Types, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-z\d_.-]+@[\da-z.-]+\.[a-zA-Z]{2,4}$/]
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
